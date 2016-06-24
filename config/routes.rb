843ea1cd1eeb89b9b5089cc11f5f9de795a0f73f
Enrique Carral Trevino <ecarralt@gmail.com>
2016-06-23 21:37:19 -0500

    Links to show path in HOME created and working

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/" => "posts#home", as: :posts
   get '/posts/new' => 'posts#new', as: :new_post
   post '/posts' => 'posts#create', as: :create_post
-  get '/posts/:id' => 'posts#show', as: :post
+  get '/posts/:id' => 'posts#show', as: :show
 
 
 
